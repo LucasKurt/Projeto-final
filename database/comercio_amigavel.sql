299fd64115e589432ea49faae209982d6967a56a
Lucas <lucascorte@outlook.com>
2021-01-20 06:00:25 -0300

    update

--- database/comercio_amigavel.sql
+++ database/comercio_amigavel.sql
@@ -1,16 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 20-Jan-2021 às 09:53
+-- Versão do servidor: 10.4.14-MariaDB
+-- versão do PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `comercio_amigavel`
+--
 CREATE DATABASE IF NOT EXISTS `comercio_amigavel` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `comercio_amigavel`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `anuncios`
+--
 
 CREATE TABLE `anuncios` (
   `id` int(11) NOT NULL,
   `id_vendedor` int(11) NOT NULL,
   `img` varchar(500) NOT NULL,
   `descricao` varchar(70) NOT NULL,
-  `valor` varchar(70) NOT NULL
+  `valor` varchar(70) NOT NULL,
+  `doacao` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `anuncios` (`id`, `id_vendedor`, `img`, `descricao`, `valor`) VALUES
-(2, 1, 'https://img.itdg.com.br/tdg/images/recipes/000/029/124/320646/320646_original.jpg?mode=crop&width=710&height=400', 'Bolo de laranja', 'R$ 10,00');
+--
+-- Extraindo dados da tabela `anuncios`
+--
+
+INSERT INTO `anuncios` (`id`, `id_vendedor`, `img`, `descricao`, `valor`, `doacao`) VALUES
+(2, 1, 'download.jpg', 'Bolo de cenoura', '25,00', 'Aceito receber doação'),
+(8, 1, '7f358d99-5294-432d-826b-0a5ce0f169ee.png', 'Guaraná Natu ', '3,99', ''),
+(10, 1, 'bolo.jpg', 'bolo de festa', '50,00', 'Aceito receber doação'),
+(11, 1, 'fanta lata.jpg', 'Fanta laranja', '5,00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
 
 CREATE TABLE `cliente` (
   `id` int(11) NOT NULL,
@@ -19,8 +61,18 @@ CREATE TABLE `cliente` (
   `senha` char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `cliente`
+--
+
 INSERT INTO `cliente` (`id`, `nome`, `email`, `senha`) VALUES
 (1, 'Lucão da Villa', 'lucao@outlook.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendedor`
+--
 
 CREATE TABLE `vendedor` (
   `id` int(11) NOT NULL,
@@ -34,17 +86,34 @@ CREATE TABLE `vendedor` (
   `img_perfil` varchar(500) NOT NULL DEFAULT './images/perfil-sem-foto.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `vendedor` (`id`, `nome`, `negocio`, `endereco`, `cpf`, `telefone`, `email`, `senha`, `img_perfil`) VALUES
-(1, 'Lucas Corte Galdino de Freitas', '', 'Rua Zurich Número 34', '111.111.111-11', '(11) 33262-800', 'lucas@outlook.com', 'e10adc3949ba59abbe56e057f20f883e', 'https://avatars3.githubusercontent.com/u/22542558?s=460&u=df058dd33c0746268fed5eae488762fa24ce2765&v=4');
+--
+-- Extraindo dados da tabela `vendedor`
+--
 
+INSERT INTO `vendedor` (`id`, `nome`, `negocio`, `endereco`, `cpf`, `telefone`, `email`, `senha`, `img_perfil`) VALUES
+(1, 'Lucas Corte Galdino de Freitas', 'Venda de Refri e Bolo', 'Rua Zurich Número 34', '111.111.111-11', '(11) 33262-800', 'lucas@outlook.com', 'e10adc3949ba59abbe56e057f20f883e', 'download.jpg');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `anuncios`
+--
 ALTER TABLE `anuncios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_vendedor` (`id_vendedor`);
 
+--
+-- Índices para tabela `cliente`
+--
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- Índices para tabela `vendedor`
+--
 ALTER TABLE `vendedor`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
@@ -52,15 +121,39 @@ ALTER TABLE `vendedor`
   ADD UNIQUE KEY `Telefone` (`telefone`),
   ADD UNIQUE KEY `negocio` (`negocio`);
 
-ALTER TABLE `anuncios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
 
+--
+-- AUTO_INCREMENT de tabela `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
 ALTER TABLE `cliente`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
 
+--
+-- AUTO_INCREMENT de tabela `vendedor`
+--
 ALTER TABLE `vendedor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `anuncios`
+--
 ALTER TABLE `anuncios`
   ADD CONSTRAINT `fk_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedor` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/publicar.php
+++ php/publicar.php
@@ -2,152 +2,158 @@
 session_start();
 require_once('bd_connect.php');
 
-print_r($_POST);
+if (isset($_POST['id_anuncio'])) {
+    #put    
+    $descricao  = $_POST['descricao'];
+    $valor = $_POST['valor'];
+    $id = $_POST['id_anuncio'];
+    $target_name = $_FILES["fileUpload"]["name"];
+    // Set image placement folder
+    $target_dir = "../images/imagens_anuncios/";
+    // Get file path
+    $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+    // Get file extension
+    $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Allowed file types
+    $allowd_file_ext = array("jpg", "jpeg", "png","webp");
 
-if (isset($_POST['doacao'])) {
-    echo 'olha deu bom';
+    if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
+        if ($descricao != "" && $valor != "") {
+            $sql = "UPDATE anuncios SET descricao = '$descricao', valor = '$valor' WHERE id='$id'";
+            $result = $conn->query($sql);
+            echo "
+                <script>
+                    alert('Sua publicação foi feita com sucesso!')
+                    location.href = '../perfil.php'
+                </script>
+            ";
+        } else {
+            echo "
+                <script>
+                    location.href = '../perfil.php'
+                </script>
+            ";
+            $resMessage = array(
+                "status" => "alert-danger",
+                "message" => "Image coudn't be uploaded."
+            );
+        }
+    } else if (!in_array($imageExt, $allowd_file_ext)) {
+        $resMessage = array(
+            "status" => "alert-danger",
+            "message" => "Allowed file formats .jpg, .jpeg and .png."
+        );            
+    } else if ($_FILES["fileUpload"]["size"] > 2097152) {
+        $resMessage = array(
+            "status" => "alert-danger",
+            "message" => "File is too large. File size should be less than 2 megabytes."
+        );
+    } else if (file_exists($target_file)) {
+        $resMessage = array(
+            "status" => "alert-danger",
+            "message" => "File already exists."
+        );
+    } else {
+        if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file) && $descricao != "" && $valor != "") {
+            $sql = "UPDATE anuncios SET img = '$target_name', descricao = '$descricao', valor = '$valor' WHERE id='$id'";
+            $result = $conn->query($sql);
+            echo "
+                <script>
+                    alert('Sua publicação foi feita com sucesso!')
+                    location.href = '../perfil.php'
+                </script>
+            ";
+        } else {
+            echo "
+                <script>
+                    location.href = '../perfil.php'
+                </script>
+            ";
+            $resMessage = array(
+                "status" => "alert-danger",
+                "message" => "Image coudn't be uploaded."
+            );
+        }
+    }
+
+} else if (isset($_POST['deletar'])){
+    #delete
+    $id = $_POST['deletar'];
+    $sql = "DELETE FROM anuncios WHERE id = '$id'";
+    $result = $conn->query($sql);
+    echo "
+        <script>
+            alert('Sua publicação foi deletada com sucesso!')
+            location.href = '../perfil.php'
+        </script>
+    ";
+} else {
+    #post
+    $id_vendedor = $_SESSION['id_vendedor'];
+    $descricao  = $_POST['descricao'];
+    $valor = $_POST['valor'];
+    
+    $target_name = $_FILES["fileUpload"]["name"];
+    // Set image placement folder
+    $target_dir = "../images/imagens_anuncios/";
+    // Get file path
+    $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+    // Get file extension
+    $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Allowed file types
+    $allowd_file_ext = array("jpg", "jpeg", "png","webp");
+    if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
+        print_r($resMessage = array(
+            "status" => "alert-danger",
+            "message" => "Select image to upload."
+        ));
+    } else if (!in_array($imageExt, $allowd_file_ext)) {
+        print_r($resMessage = array(
+            "status" => "alert-danger",
+            "message" => "Allowed file formats .jpg, .jpeg and .png."
+        ));            
+    } else if ($_FILES["fileUpload"]["size"] > 2097152) {
+        print_r($resMessage = array(
+            "status" => "alert-danger",
+            "message" => "File is too large. File size should be less than 2 megabytes."
+        ));
+    } else if (file_exists($target_file)) {
+        print_r($resMessage = array(
+            "status" => "alert-danger",
+            "message" => "File already exists."
+        ));
+    } else {
+        if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file) && $descricao != "" && $valor != "") {
+            if (isset($_POST['doacao'])) {
+                $doacao = "Aceito receber doação";
+                $sql = "INSERT INTO anuncios (id_vendedor, img, descricao, valor, doacao) VALUES ('$id_vendedor', '$target_name', '$descricao', '$valor', '$doacao')";
+                $result = $conn->query($sql);
+                echo "
+                <script>
+                    alert('Sua publicação foi feita com sucesso!')
+                    location.href = '../perfil.php'
+                </script>
+            ";
+            } else {
+                $sql = "INSERT INTO anuncios (id_vendedor, img, descricao, valor) VALUES ('$id_vendedor', '$target_name', '$descricao', '$valor')";
+                $result = $conn->query($sql);
+                echo "
+                <script>
+                    alert('Sua publicação foi feita com sucesso!')
+                    location.href = '../perfil.php'
+                </script>
+            ";
+            }
+        } else {
+            echo "
+                <script>
+                    location.href = '../perfil.php'
+                </script>
+            ";
+            print_r($resMessage = array(
+                "status" => "alert-danger",
+                "message" => "Image coudn't be uploaded."
+            ));
+        }
+    }
 }
-
-// if (isset($_POST['id_anuncio'])) {
-//     #put    
-//     $descricao  = $_POST['descricao'];
-//     $valor = $_POST['valor'];
-//     $id = $_POST['id_anuncio'];
-//     $target_name = $_FILES["fileUpload"]["name"];
-//     // Set image placement folder
-//     $target_dir = "../images/imagens_anuncios/";
-//     // Get file path
-//     $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
-//     // Get file extension
-//     $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-//     // Allowed file types
-//     $allowd_file_ext = array("jpg", "jpeg", "png","webp");
-
-//     if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
-//         if ($descricao != "" && $valor != "") {
-//             $sql = "UPDATE anuncios SET descricao = '$descricao', valor = '$valor' WHERE id='$id'";
-//             $result = $conn->query($sql);
-//             echo "
-//                 <script>
-//                     alert('Sua publicação foi feita com sucesso!')
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//         } else {
-//             echo "
-//                 <script>
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//             $resMessage = array(
-//                 "status" => "alert-danger",
-//                 "message" => "Image coudn't be uploaded."
-//             );
-//         }
-//     } else if (!in_array($imageExt, $allowd_file_ext)) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "Allowed file formats .jpg, .jpeg and .png."
-//         );            
-//     } else if ($_FILES["fileUpload"]["size"] > 2097152) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "File is too large. File size should be less than 2 megabytes."
-//         );
-//     } else if (file_exists($target_file)) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "File already exists."
-//         );
-//     } else {
-//         if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file) && $descricao != "" && $valor != "") {
-//             $sql = "UPDATE anuncios SET img = '$target_name', descricao = '$descricao', valor = '$valor' WHERE id='$id'";
-//             $result = $conn->query($sql);
-//             echo "
-//                 <script>
-//                     alert('Sua publicação foi feita com sucesso!')
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//         } else {
-//             echo "
-//                 <script>
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//             $resMessage = array(
-//                 "status" => "alert-danger",
-//                 "message" => "Image coudn't be uploaded."
-//             );
-//         }
-//     }
-
-// } else if (isset($_POST['deletar'])){
-//     #delete
-//     $id = $_POST['deletar'];
-//     $sql = "DELETE FROM anuncios WHERE id = '$id'";
-//     $result = $conn->query($sql);
-//     echo "
-//         <script>
-//             alert('Sua publicação foi deletada com sucesso!')
-//             location.href = '../perfil.php'
-//         </script>
-//     ";
-// } else {
-//     #post
-//     $id_vendedor = $_SESSION['id_vendedor'];
-//     $descricao  = $_POST['descricao'];
-//     $valor = $_POST['valor'];
-//     $target_name = $_FILES["fileUpload"]["name"];
-//     // Set image placement folder
-//     $target_dir = "../images/imagens_anuncios/";
-//     // Get file path
-//     $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
-//     // Get file extension
-//     $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-//     // Allowed file types
-//     $allowd_file_ext = array("jpg", "jpeg", "png","webp");
-
-//     if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "Select image to upload."
-//         );
-//     } else if (!in_array($imageExt, $allowd_file_ext)) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "Allowed file formats .jpg, .jpeg and .png."
-//         );            
-//     } else if ($_FILES["fileUpload"]["size"] > 2097152) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "File is too large. File size should be less than 2 megabytes."
-//         );
-//     } else if (file_exists($target_file)) {
-//         $resMessage = array(
-//             "status" => "alert-danger",
-//             "message" => "File already exists."
-//         );
-//     } else {
-//         if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file) && $descricao != "" && $valor != "") {
-//             $sql = "INSERT INTO anuncios (id_vendedor, img, descricao, valor) VALUES ('$id_vendedor', '$target_name', '$descricao', '$valor')";
-//             $result = $conn->query($sql);
-//             echo "
-//                 <script>
-//                     alert('Sua publicação foi feita com sucesso!')
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//         } else {
-//             echo "
-//                 <script>
-//                     location.href = '../perfil.php'
-//                 </script>
-//             ";
-//             $resMessage = array(
-//                 "status" => "alert-danger",
-//                 "message" => "Image coudn't be uploaded."
-//             );
-//         }
-//     }
-// }
