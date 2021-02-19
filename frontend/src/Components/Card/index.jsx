36e8e9a735e3c5ba8c3599068d3171a0e2ff782b
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-19 17:24:31 -0300

    new files

--- frontend/src/Components/Card/index.jsx
+++ frontend/src/Components/Card/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Card = ({ img, imgPerfil, descricao, valor, opc, doacao , put }) => {
-    if (!img) {
-        img =  '/images/imagens_anuncios/ponto-de-interrogação.jpeg'
-    }
-    if (put) {
-        img = `http://localhost:3333/uploads/${img}`
-    }
+    // if (!img) {
+    //     img =  '/images/imagens_anuncios/ponto-de-interrogação.jpeg'
+    // }
+    // if (put) {
+    //     img = `http://localhost:3333/uploads/${img}`
+    // }
 
     console.log(img)
 
@@ -14,7 +14,7 @@ const Card = ({ img, imgPerfil, descricao, valor, opc, doacao , put }) => {
         <div className="card mb-4 shadow-sm">
             <img id="imgPlaceholder" height={225} src={img} alt="Imagem do anuncio" />
 
-            <div className="d-flex justify-content-center mt-3"><img style={{ boxShadow: '0px 0px 8px #000000' }} className="rounded-circle" width={50} height={50} src={`/images/perfil_vendedor/${imgPerfil}`} alt="Perfil vendedor" /></div>
+            <div className="d-flex justify-content-center mt-3"><img style={{ boxShadow: '0px 0px 8px #000000' }} className="rounded-circle" width={50} height={50} src={imgPerfil} alt="Perfil vendedor" /></div>
             <div className="card-body">
                 <p className="card-text text-center">{/*?php echo $negocio?*/}</p>
                 <p id="descricaoAnuncio" className="card-text text-center">{descricao === '' ? 'O que? (breve descrição)' : descricao}</p>
