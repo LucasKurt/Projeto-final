00f97076ff5bdb6ddaee95d5aa0b113e8c98959f
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-19 16:29:02 -0300

    reducer again

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,6 @@
 import {createStore} from 'redux';
 
-<<<<<<< HEAD
-import reducers from '../Reducers';
-=======
 import reducers from '../reducers';
->>>>>>> 486e844f03ac345d9255093435b9c879b5d5d3db
 
 const store = createStore(
     reducers,
